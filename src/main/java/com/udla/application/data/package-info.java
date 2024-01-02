270e42b475d02fa66903cb74d7e66d0e2fd7410b
start.vaadin.com <no-reply@vaadin.com>
2024-01-02 12:49:34 +0000

    Generated project

---
+++ src/main/java/com/udla/application/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.udla.application.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/udla/application/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.udla.application.services;
+
+import org.springframework.lang.NonNullApi;
